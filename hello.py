788dce18b0282f7334e1f376aaf82c76a1d8f7a5
Seong Joo <seongjooyoon@gmail.com>
2023-09-19 17:53:40 -0400

    Implemented bootstrap template to webpage and added a live date/time display to the home page

--- hello.py
+++ hello.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<seongjoo>')
 def user(seongjoo):
-    return '<h1>Hello {}!</h1>'.format(seongjoo)
+    return render_template('user.html', name='Seong Joo')
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
