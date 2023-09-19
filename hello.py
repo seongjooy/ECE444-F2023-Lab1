1bbb42743984a2580ac8e599012393e94404bb34
Seong Joo <seongjooyoon@gmail.com>
2023-09-19 11:41:36 -0400

    Added exercise 2-2

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<seongjoo>')
+def user(seongjoo):
+    return '<h1>Hello {}!</h1>'.format(seongjoo)
